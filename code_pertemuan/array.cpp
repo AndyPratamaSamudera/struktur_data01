525aa2fa25e627edf20c54e505b02f2a574d4171
Andy Pratamasamudera <andypratamasamudera@gmail.com>
2026-06-11 01:34:54 +0700

    array

---
+++ code_pertemuan/array.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int daftar_angka[3] = {1, 2, 3};
+    for (int i = 0; i < 3; i++) {
+        cout << daftar_angka[i] << endl;
+    }
+    return 0;
+}
